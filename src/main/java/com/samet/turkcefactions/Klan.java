3381506c4499ce88a74d518ddd65a05f2b6b4d68
samibozkurt0198-dev <samibozkurt0198@gmail.com>
2026-09-07 13:25:28 +0300

    Update Klan.java

--- src/main/java/com/samet/turkcefactions/Klan.java
+++ src/main/java/com/samet/turkcefactions/Klan.java
@@ -1,7 +1,6 @@
 package com.samet.turkcefactions;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,10 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
@@ -28,7 +24,6 @@ public class Klan extends JavaPlugin implements CommandExecutor, Listener {
 
     private final Map<UUID, String> klanDavetleri = new HashMap<>();
 
-    // Market Urun Tanimlari
     public static class ShopItem {
         public String displayName;
         public Material material;
@@ -55,13 +50,9 @@ public class Klan extends JavaPlugin implements CommandExecutor, Listener {
             getDataFolder().mkdirs();
         }
 
-        // Market urunlerini yukle
         initShopItems();
-
-        // Etkinlik Dinleyicilerini Kaydet
         getServer().getPluginManager().registerEvents(this, this);
 
-        // Komutlari Kaydet
         registerCommand("klan");
         registerCommand("arsa");
         registerCommand("kit");
@@ -72,9 +63,7 @@ public class Klan extends JavaPlugin implements CommandExecutor, Listener {
         registerCommand("para");
         registerCommand("pay");
 
-        // Scoreboard Guncelleme Gorevi (Her 3 saniyede bir yeniler)
         Bukkit.getScheduler().runTaskTimer(this, this::updateAllScoreboards, 20L, 60L);
-
         getLogger().info("§aTurkce Bedrock Factions, Arsa, Market & Ekonomi Eklentisi Aktif!");
     }
 
@@ -85,36 +74,25 @@ public class Klan extends JavaPlugin implements CommandExecutor, Listener {
     }
 
     private void initShopItems() {
-        // Bloklar
         shopItems.add(new ShopItem("Tas x64", Material.STONE, 64, 100, "Bloklar", "https://textures.minecraft.net/texture/a18a287a206a4b159f81f1e8a834e56d43e597c5f3b7933f1f3a22c53a6e3"));
         shopItems.add(new ShopItem("Cimen x64", Material.GRASS_BLOCK, 64, 150, "Bloklar", "https://textures.minecraft.net/texture/1f31f99c85df230a133df1b312384a6c6c7471804c4b5bc7f0bf18df32f8373"));
         shopItems.add(new ShopItem("Mese Odunu x64", Material.OAK_LOG, 64, 200, "Bloklar", "https://textures.minecraft.net/texture/b898a39a3f28cfdf5c68b75960d7d6f5f3e9c71bc458a25c1e5d7718e38d72"));
         shopItems.add(new ShopItem("Obsidyen x16", Material.OBSIDIAN, 16, 500, "Bloklar", "https://textures.minecraft.net/texture/2f8b50e18d6e3c1626f21c27e857850a4d538e1b0c950d8df302e1c9533a1e2"));
-
-        // Madenler & Esyalar
         shopItems.add(new ShopItem("Elmas x5", Material.DIAMOND, 5, 1000, "Madenler", "https://textures.minecraft.net/texture/a31a980562e15119bb989a3a91e5e54d632f122e23d7f76371c1f4e1f7d5c7"));
         shopItems.add(new ShopItem("Demir Kule x16", Material.IRON_INGOT, 16, 300, "Madenler", "https://textures.minecraft.net/texture/26197116a41f8615c4d081f2f01f8d8c2c11ee14df1d368d1f8bc63402324e"));
         shopItems.add(new ShopItem("Altin Kule x16", Material.GOLD_INGOT, 16, 600, "Madenler", "https://textures.minecraft.net/texture/d67e0c388274384a123610fb176b6255d6428c943f7a1f6a1c1d0b7e2a9"));
         shopItems.add(new ShopItem("Zümrüt x5", Material.EMERALD, 5, 800, "Madenler", "https://textures.minecraft.net/texture/238a8e1b5f257a3e1a0b5c1c8a14352b2b3a1a9e1d8a1c9e8a7f1a9a8b1c2d3"));
-
-        // Yemekler
         shopItems.add(new ShopItem("Biftek x32", Material.COOKED_BEEF, 32, 120, "Yemekler", "https://textures.minecraft.net/texture/416323f81504221191060938a168a719c8173400a454d6d1d4d802ed3a2b7e9"));
         shopItems.add(new ShopItem("Altin Elma x2", Material.GOLDEN_APPLE, 2, 800, "Yemekler", "https://textures.minecraft.net/texture/b05688773923ea505745fc7e6168e0e09e3be31d816048556cef289eb4e1d"));
         shopItems.add(new ShopItem("Büyülü Elma x1", Material.ENCHANTED_GOLDEN_APPLE, 1, 5000, "Yemekler", "https://textures.minecraft.net/texture/5f29910e5b0b10b3ef18b375be8540329616d2861e68da08512ccb4415e1926d"));
-
-        // Ekipman
         shopItems.add(new ShopItem("Elmas Kilic", Material.DIAMOND_SWORD, 1, 1500, "Ekipman", "https://textures.minecraft.net/texture/a18a287a206a4b159f81f1e8a834e56d43e597c5f3b7933f1f3a22c53a6e3"));
         shopItems.add(new ShopItem("Elmas Kazma", Material.DIAMOND_PICKAXE, 1, 1500, "Ekipman", "https://textures.minecraft.net/texture/2f8b50e18d6e3c1626f21c27e857850a4d538e1b0c950d8df302e1c9533a1e2"));
     }
 
-    // ==========================================
-    // EKONOMİ SİSTEMİ
-    // ==========================================
-
     public double getBalance(Player player) {
         String uuid = player.getUniqueId().toString();
         if (!getConfig().contains("ekonomi." + uuid)) {
-            getConfig().set("ekonomi." + uuid, 1000.0); // Baslangic Parasi
+            getConfig().set("ekonomi." + uuid, 1000.0);
             saveConfig();
         }
         return getConfig().getDouble("ekonomi." + uuid);
@@ -139,10 +117,6 @@ public class Klan extends JavaPlugin implements CommandExecutor, Listener {
         }
         return false;
     }
-
-    // ==========================================
-    // SCOREBOARD SİSTEMİ ( Sag Ust Tablo )
-    // ==========================================
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
@@ -177,10 +151,6 @@ public class Klan extends JavaPlugin implements CommandExecutor, Listener {
 
         player.setScoreboard(board);
     }
-
-    // ==========================================
-    // KOMUT İŞLEYİCİ
-    // ==========================================
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -222,7 +192,8 @@ public class Klan extends JavaPlugin implements CommandExecutor, Listener {
             return true;
         }
 
-        if (!FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) {
+        // Floodgate yüklü olduğu takdirde oyuncunun mobil mi Java mı olduğunu kontrol eder
+        if (Bukkit.getPluginManager().isPluginEnabled("Floodgate") && !FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) {
             player.sendMessage("§cBu menuyu sadece Bedrock/Mobil oyunculari kullanabilir.");
             return true;
         }
@@ -237,10 +208,6 @@ public class Klan extends JavaPlugin implements CommandExecutor, Listener {
 
         return true;
     }
-
-    // ==========================================
-    // MARKET & ARAMA SİSTEMİ (Bedrock UI)
-    // ==========================================
 
     private void openMarketAnaMenusu(Player player) {
         SimpleForm.Builder form = SimpleForm.builder()
@@ -348,17 +315,8 @@ public class Klan extends JavaPlugin implements CommandExecutor, Listener {
         }
     }
 
-    // --- DIĞER SISTEMLER (Klan, Arsa Korumasi, Kitler) ---
-    // (Önceki kod yapısının aynısı muhafaza edilmiştir)
-    private void openAnaKlanMenusu(Player player) { /* ... */ }
-    private void openKlanKurForm(Player player) { /* ... */ }
-    private void setKlanHome(Player player, String klanAdi) { /* ... */ }
-    private void klanHomeIsinlan(Player player, String klanAdi) { /* ... */ }
-    private void openDavetEtForm(Player player, String klanAdi) { /* ... */ }
-    private void openDavetMenusu(Player player) { /* ... */ }
-    private void showKlanInfo(Player player, String klanAdi) { /* ... */ }
-    private void klandanAyril(Player player) { /* ... */ }
-    private void handleKlanChat(Player player, String[] args) { /* ... */ }
-    private void openArsaMenusu(Player player) { /* ... */ }
-    private void openKitMenusu(Player player) { /* ... */ }
+    private void openAnaKlanMenusu(Player player) { }
+    private void handleKlanChat(Player player, String[] args) { }
+    private void openArsaMenusu(Player player) { }
+    private void openKitMenusu(Player player) { }
 }
